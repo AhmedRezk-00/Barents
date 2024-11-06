8c617f0825db8ed0f211a06129f7ea2be5fe8b63
Luke M <116373026+akaaa128@users.noreply.github.com>
2024-11-06 02:19:34 +0100

    Update revised-graphManager.py
    changed function order

--- revised-graphManager.py
+++ revised-graphManager.py
@@ -15,11 +15,21 @@ uniqueId = 0
 # function to export rdf graph as .xml
 def exportGraph():
     mainGraph.serialize(format="xml", destination="resultsRDF.xml")
+def resetGraph():
+    global mainGraph
+    del mainGraph
+    mainGraph = rdf.Graph()
+    mainGraph.bind("dl", dl)
 
 # function to add literals, which can both represent data zones and data sources (as in data lakes, banks, etc.) 
 def addLiteral(literal):
     # perhaps use rdflib containers here instead
     literals.append(rdf.Literal(literal))
+def renameTriples(ressource, newName):
+    for s, p, o in mainGraph.triples((rdf.URIRef(dl + ressource), None, None)):
+        mainGraph.remove((s,p,o))
+        # add check if newname is avialble
+        mainGraph.add((rdf.URIRef(dl + newName),p,o))
 
 # function to add data layer ressource
 def addDataSource():
@@ -27,45 +37,28 @@ def addDataSource():
     global uniqueId
     mainGraph.add((rdf.URIRef(dl + "unnamedRessource" + str(uniqueId)), dl.layer, dataLevel))
     uniqueId += 1
-
-def renameTriples(ressource, newName):
-    for s, p, o in mainGraph.triples((rdf.URIRef(dl + ressource), None, None)):
-        mainGraph.remove((s,p,o))
-        # add check if newname is avialble
-        mainGraph.add((rdf.URIRef(dl + newName),p,o))
-
-def addSourceToResource(name, source):
-    mainGraph.add((rdf.URIRef(dl + name), dl.source, rdf.Literal(source)))
-
-def addTransformation():
-    # add proper check for duplicate names
-    global uniqueId
-    mainGraph.add((rdf.URIRef(dl + "unnamedTransformation" + str(uniqueId)), dl.layer, informationLevel))
-    uniqueId += 1
-
-def setTransformationType(subject, transformationType):
-    mainGraph.set((rdf.URIRef(dl + subject), dl.type, rdf.Literal(transformationType)))
-
-def setPartOf(subject, object):
-    mainGraph.set((rdf.URIRef(dl + subject), dl.partOf, rdf.URIRef(dl + object)))
-
-def setTransformationFunction(subject, function):
-    mainGraph.set((rdf.URIRef(dl + subject), dl.function, rdf.Literal(function)))
-
 def addDataSink():
     # add proper check for duplicate names
     global uniqueId
     mainGraph.add((rdf.URIRef(dl + "unnamedRessource" + str(uniqueId)), dl.layer, knowledgeLevel))
     uniqueId += 1
 
+def addTransformation():
+    # add proper check for duplicate names
+    global uniqueId
+    mainGraph.add((rdf.URIRef(dl + "unnamedTransformation" + str(uniqueId)), dl.layer, informationLevel))
+    uniqueId += 1
+def setTransformationType(subject, transformationType):
+    mainGraph.set((rdf.URIRef(dl + subject), dl.type, rdf.Literal(transformationType)))
+def setTransformationFunction(subject, function):
+    mainGraph.set((rdf.URIRef(dl + subject), dl.function, rdf.Literal(function)))
+
 def setZone(subject, object):
     mainGraph.set((rdf.URIRef(dl + subject), dl.zone, rdf.Literal(object)))
-
-def resetGraph():
-    global mainGraph
-    del mainGraph
-    mainGraph = rdf.Graph()
-    mainGraph.bind("dl", dl)
+def setPartOf(subject, object):
+    mainGraph.set((rdf.URIRef(dl + subject), dl.partOf, rdf.URIRef(dl + object)))
+def addSourceToResource(name, source):
+    mainGraph.add((rdf.URIRef(dl + name), dl.source, rdf.Literal(source)))
 
 # code-only example to generate ontology from barents paper to cover core functionality 
 def testRun():
