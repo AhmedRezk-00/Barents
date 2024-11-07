3cfb719295c5bffbe7c47d1c1850c09cf1d796a6
Luke M <st177937@stud.uni-stuttgart.de>
2024-11-08 00:28:46 +0100

    added frontend entry point

---
+++ prototype/frontend/frontend.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry('1200x675')
+root.minsize(800,450)
+root.state('zoomed')
+root.configure(bg='gray10')
+root.title("BFP-Barents frontend")
+
+root.mainloop()
