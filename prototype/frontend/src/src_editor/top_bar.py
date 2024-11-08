576cba606c60449bbfcd286ebc164d0023f664f4
Luke M <st177937@stud.uni-stuttgart.de>
2024-11-08 18:56:40 +0100

    frontend: added export button in top bar

--- prototype/frontend/src/src_editor/top_bar.py
+++ prototype/frontend/src/src_editor/top_bar.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+from src.rdf_manager import export_graph
 
 def create_top_bar(root):
-    pass
+    # create grid layout of top_bar
+    root.columnconfigure((0,1,2,3,4,5,6,7), weight=1)
+    root.rowconfigure(0, weight=1)
+
+    # define an export button that exports the rdf graph as an xml file
+    export_button = tk.Button(command=(lambda: export_graph('test')))
+    export_button.grid(row=0, column=0, sticky='nesw')
