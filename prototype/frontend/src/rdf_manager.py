653e857dd0838a19d5bc96704c1db5e37990a38b
Luke M <st177937@stud.uni-stuttgart.de>
2024-11-08 19:44:17 +0100

    frontend: updated graph_manager based on mockup
    moved rough outline and some function definitions from mockups to prototype graph_manager

--- prototype/frontend/src/rdf_manager.py
+++ prototype/frontend/src/rdf_manager.py
@@ -6,6 +6,65 @@ rdf_graph = rdf.Graph()
 dl = rdf.Namespace("http://barents.dl/")
 rdf_graph.bind("dl", dl)
 
+# id that should match id of items on canvas. it should be unique for all created rdf subjects
+canvas_id = 1
+
+# array that stores all resources created in rdf graph at any given canvas_id 
+resource_dictionary = []
+
+# variables to store rdf literal of layer information to ensure consistency
+knowledge_layer = rdf.Literal("Knowledge Layer")
+information_layer = rdf.Literal("Information Layer")
+data_layer = rdf.Literal("Data Layer")
+
 # function to export rdf_graph as an .xml file 
 def export_graph(file_name):
     rdf_graph.serialize(format="xml", destination=file_name + '.xml')
+
+# function to delete all triples from rdf graph
+def reset_graph():
+    pass
+
+# function to add data level resource
+def add_data_source():
+    pass
+
+# function to add knowledge level resource
+def add_data_sink():
+    pass
+
+# function to ass information level resource
+def add_transformation():
+    pass
+
+# function to set type of given transformation
+def set_transformation_type():
+    pass
+
+# function to set expression that defines given transformation
+def set_transformation_expression():
+    pass
+
+# function to rename all triples belonging to a given subject
+def rename_triples():
+    pass
+
+# helper function to ensure new resources are added properly to dictionary
+def add_to_dictionary():
+    pass
+
+# function that returns the level at which a given resource belongs. 
+def return_level():
+    pass
+
+# function to set the zone literal of a given knowledge level resource
+def set_zone():
+    pass
+
+# function to set partof relationship given two resources, with one of them being a information level resource (transformation)
+def set_part_of():
+    pass
+
+# function to set source literal of a given data level resource
+def set_source():
+    pass
