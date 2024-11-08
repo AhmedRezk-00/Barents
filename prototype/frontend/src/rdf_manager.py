7578577a21b36f8596beaa9d41dc75de3e4158f0
Luke M <st177937@stud.uni-stuttgart.de>
2024-11-08 18:46:18 +0100

    frontend: added rdf_manager export function

--- prototype/frontend/src/rdf_manager.py
+++ prototype/frontend/src/rdf_manager.py
@@ -1,1 +1,11 @@
 import rdflib as rdf
+
+# initialize graph. changes made to the graph should reflect in the editor.
+rdf_graph = rdf.Graph()
+# define namespace for rdf_graph thats specific to barents
+dl = rdf.Namespace("http://barents.dl/")
+rdf_graph.bind("dl", dl)
+
+# function to export rdf_graph as an .xml file 
+def export_graph(file_name):
+    rdf_graph.serialize(format="xml", destination=file_name + '.xml')
