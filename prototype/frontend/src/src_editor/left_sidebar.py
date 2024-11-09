8ebab784922e5ccb3c1ce97bcd8356a16e9bad8e
Luke M <st177937@stud.uni-stuttgart.de>
2024-11-09 21:33:08 +0100

    frontend left sidebar skeleton
    added some tk frames to left sidebar for future use

--- prototype/frontend/src/src_editor/left_sidebar.py
+++ prototype/frontend/src/src_editor/left_sidebar.py
@@ -4,3 +4,11 @@ def create_left_sidebar(root):
     # divide left sidebar into grids to grid components
     root.columnconfigure(0, weight=1)
     root.rowconfigure((0,1,2), weight=1)
+
+    data_source_panel = tk.Frame(root)
+    data_sink_panel = tk.Frame(root)
+    transformation_panel = tk.Frame(root)
+
+    data_source_panel.grid(row=0, column=0, sticky='nesw')
+    data_sink_panel.grid(row=1, column=0, sticky='nesw')
+    transformation_panel.grid(row=2, column=0, sticky='nesw')
