1e1a325fedf6565d81285714ee61847642b358a9
Luke M <st177937@stud.uni-stuttgart.de>
2024-11-10 07:37:25 +0100

    frontend: add resource buttons, drag & drop
    - added buttons to add knowledge & information level resource
    - fixed left_sidebar panel layout to display buttons properly
    - introduced bar bones drag & drop functionality

--- prototype/frontend/src/src_editor/canvas.py
+++ prototype/frontend/src/src_editor/canvas.py
@@ -1,6 +1,12 @@
 import tkinter as tk 
 import src.shared_resources
 
+# variables to store the last clicked resource
+current_resource_id = None
+# variables to handle drag and drop
+offset_x = 0
+offset_y = 0
+
 # function to create canvas of editor. rdf graph resources should be displayed here 
 def create_canvas(root):
     # create canvas widget
@@ -11,3 +17,32 @@ def create_canvas(root):
 
     # define shared_resources.canvas to be this canvas
     src.shared_resources.canvas = canvas
+
+    # events for drag and drop & right sidebar customization 
+    canvas.bind('<Button-1>', lambda event: on_click(event, canvas))
+    canvas.bind("<B1-Motion>", lambda event: on_drag(event, canvas))
+    canvas.bind("<ButtonRelease-1>", lambda event: on_drop(event))
+
+# function to handle on_click event
+def on_click(event, canvas):
+    global current_resource, current_resource_id
+    # check if theres any items on canvas to drag and drop
+    if canvas.find_all():
+        current_resource_id = canvas.find_closest(event.x, event.y)[0]
+        global offset_x, offset_y
+        offset_x = event.x - canvas.coords(current_resource_id)[0]
+        offset_y = event.y - canvas.coords(current_resource_id)[1]
+
+# function to handle drag event
+def on_drag(event, canvas):
+    global current_resource_id
+    # check if there's any resource selected to drag
+    if current_resource_id:
+        new_x = event.x - offset_x
+        new_y = event.y - offset_y
+        canvas.coords(current_resource_id, new_x, new_y, new_x+50, new_y+50)
+
+# function to handle  drop event
+def on_drop(event): 
+    # NOTE: resetting current_resource_id would also reset right_sidebar selection unless an extra variable is introduced
+    pass
--- prototype/frontend/src/src_editor/left_sidebar.py
+++ prototype/frontend/src/src_editor/left_sidebar.py
@@ -13,15 +13,40 @@ def create_left_sidebar(root):
     data_sink_panel = tk.Frame(root)
     transformation_panel = tk.Frame(root)
 
+    # grid panels 
     data_source_panel.grid(row=0, column=0, sticky='nesw')
-    data_sink_panel.grid(row=1, column=0, sticky='nesw')
-    transformation_panel.grid(row=2, column=0, sticky='nesw')
+    data_sink_panel.grid(row=2, column=0, sticky='nesw')
+    transformation_panel.grid(row=1, column=0, sticky='nesw')
+    # split panels into appropriate grid to add components
+    data_source_panel.columnconfigure(0, weight=1)
+    data_source_panel.rowconfigure(0, weight=1)
+    data_sink_panel.columnconfigure(0, weight=1)
+    data_sink_panel.rowconfigure(0, weight=1)
+    transformation_panel.columnconfigure(0, weight=1)
+    transformation_panel.rowconfigure(0, weight=1)
 
-    # added button to add generic data level resource
+    # button to add generic data level resource
     data_source_button = tk.Button(data_source_panel, text = 'add data source', command=(lambda: add_data_source()))
     data_source_button.grid(column=0, row=0, sticky = 'nesw')
+
+    # button to add generic knowledge level resource
+    data_sink_button = tk.Button(data_sink_panel, text='add data sink', command=(lambda: add_data_sink()))
+    data_sink_button.grid(column=0, row=0, sticky='nesw')
+
+    # button to add generic information level resource
+    transformation_button = tk.Button(transformation_panel, text='add transformation', command=(lambda: add_transformation()))
+    transformation_button.grid(column=0, row=0, sticky='nesw')
 
 # function to create rectangle to represent data source on canvas and add data source to rdf graph
 def add_data_source():
     square = src.shared_resources.canvas.create_rectangle(50, 50, 100, 100, fill='gray30')
     rdf_manager.add_data_source(square)
+
+# function to add data sink to rdf graph and rectangle to represent it on canvas
+def add_data_sink():
+    square = src.shared_resources.canvas.create_rectangle(50, 50, 100, 100, fill='olive')
+    rdf_manager.add_data_sink(square)
+
+def add_transformation():
+    square = src.shared_resources.canvas.create_rectangle(50, 50, 100, 100, fill='teal')
+    rdf_manager.add_transformation(square)
