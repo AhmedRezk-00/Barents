407e0690773bdbd02657e8bd418bec42ecd060fa
Luke M <st177937@stud.uni-stuttgart.de>
2024-11-10 08:02:45 +0100

    frontend: core functions to update right_sidebar
    - added right_sidebar testLabel that updates dynamically based on layer of selected resource

--- prototype/frontend/src/src_editor/canvas.py
+++ prototype/frontend/src/src_editor/canvas.py
@@ -1,5 +1,7 @@
 import tkinter as tk 
 import src.shared_resources
+from src.rdf_manager import resource_dictionary, get_level
+from src.src_editor.right_sidebar import update_right_sidebar
 
 # variables to store the last clicked resource
 current_resource_id = None
@@ -25,13 +27,16 @@ def create_canvas(root):
 
 # function to handle on_click event
 def on_click(event, canvas):
-    global current_resource, current_resource_id
+    global current_resource_id
     # check if theres any items on canvas to drag and drop
     if canvas.find_all():
         current_resource_id = canvas.find_closest(event.x, event.y)[0]
         global offset_x, offset_y
         offset_x = event.x - canvas.coords(current_resource_id)[0]
         offset_y = event.y - canvas.coords(current_resource_id)[1]
+
+        # update right sidebar 
+        update_right_sidebar(get_level(resource_dictionary[current_resource_id]))
 
 # function to handle drag event
 def on_drag(event, canvas):
@@ -46,3 +51,7 @@ def on_drag(event, canvas):
 def on_drop(event): 
     # NOTE: resetting current_resource_id would also reset right_sidebar selection unless an extra variable is introduced
     pass
+
+# function to return current_resource_id 
+def get_current_resource_id():
+    return current_resource_id
--- prototype/frontend/src/src_editor/right_sidebar.py
+++ prototype/frontend/src/src_editor/right_sidebar.py
@@ -1,4 +1,13 @@
 import tkinter as tk
 
 def create_right_sidebar(root):
-    pass
+    root.rowconfigure(0, weight=1)
+    root.columnconfigure(0, weight=1)
+
+    global testText
+    testText = tk.StringVar()
+    testLabel = tk.Label(root, textvariable=testText)
+    testLabel.grid(row=0, column=0, sticky='nesw')
+
+def update_right_sidebar(resource):
+    testText.set(resource)
