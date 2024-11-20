7e685f479df4ff65d959c7c66dff53530326ddc0
Luke M <st177937@stud.uni-stuttgart.de>
2024-11-20 06:08:24 +0100

    fixed size for sidepanels and top bar
    changed dynamic grid sizes to make sure sidebars and top bar have consistent size

--- prototype/frontend/frontend.py
+++ prototype/frontend/frontend.py
@@ -4,7 +4,7 @@ from src.editor import create_editor
 # define root window size, color and title
 root = ctk.CTk()
 root.geometry('1200x675')
-root.minsize(800,450)
+root.minsize(900,500)
 root.state('zoomed')
 root.configure(bg='gray10')
 root.title("BFP-Barents frontend")
