f687ddb5e5dc2e1d1f5564c7b181a71516588bb2
Luke M <st177937@stud.uni-stuttgart.de>
2024-12-04 06:56:39 +0100

    Update backend.py

--- prototype/backend/backend.py
+++ prototype/backend/backend.py
@@ -17,10 +17,19 @@ def process_rdf(rdf_location):
     rdf_graph.parse(rdf_location)
     sources = []
 
-    for s, p, o in rdf_graph.triples((None, dl.layer, knowledge_layer)):
-        if rdf_graph.value(s, dl.dbLocation):
-            conn =sql.connect(rdf_graph.value(s, dl.dbLocation))
-            conn.close()
+    # iterate over all sinks 
+    for ss, sp, so in rdf_graph.triples((None, dl.layer, knowledge_layer)):
+        if rdf_graph.value(ss, dl.dbLocation):
+            sink_conn =sql.connect(rdf_graph.value(ss, dl.dbLocation))
+            # iterate over all transformations that lead into those sinks 
+            for ts, tp, to in rdf_graph.triples((None, dl.partOf, ss)):
+                # iterate over all sourcers that lead into those transformation
+                for s, p, o in rdf_graph.triples((None, dl.partOf, ts)):
+                    # if the current transformation is a filter
+                    transformation_type = rdf_graph.value(ts, dl.type)
+                    if transformation_type == 'filter':
+                        pass
+            sink_conn.close()
 
 process_rdf(rdf_location)
 
